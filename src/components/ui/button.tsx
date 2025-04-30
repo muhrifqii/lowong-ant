30513bee98b92c1381ead5a16f719d5811a540d1
Muhammad Rifqi Fatchurrahman <muh_rif@live.com>
2025-04-30 12:03:53 +0700

    custom button with loading

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { Loader2 } from "lucide-react"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -35,16 +36,15 @@ const buttonVariants = cva(
   }
 )
 
+type ButtonProps = React.ComponentProps<"button"> & VariantProps<typeof buttonVariants> & { asChild?: boolean };
+
 function Button({
   className,
   variant,
   size,
   asChild = false,
   ...props
-}: React.ComponentProps<"button"> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
+}: ButtonProps) {
   const Comp = asChild ? Slot : "button"
 
   return (
@@ -56,4 +56,24 @@ function Button({
   )
 }
 
-export { Button, buttonVariants }
+function LoadingButton({
+  isLoading,
+  loadingText = "Please wait",
+  children,
+  disabled,
+  ...props
+}: ButtonProps & { isLoading?: boolean, loadingText?: string }) {
+  return (
+    <Button {...props} disabled={disabled || isLoading}>
+      {isLoading ?
+        (<>
+          <Loader2 className="animate-spin"/> {loadingText}
+        </>)
+        :
+        children
+      }
+    </Button>
+  )
+}
+
+export { Button, LoadingButton, buttonVariants }
